a6565493aafdc48d893585aa0bd6ae6e27219e15
afnan030503 <muhammadafnanmaulidi@gmail.com>
2023-03-27 14:24:02 +0700

    Add project files.

---
+++ contoh/contoh.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	jumlah_pendaftar;
+
+	cout << "Masukkan jumlah pendaftar: ";
+	cin >> jumlah_pendaftar;
+
+	cout << "Jumlah pendaftar adalah: " << jumlah_pendaftar << endl;
+
+	return "jumlah_pendaftar : ";
+}
